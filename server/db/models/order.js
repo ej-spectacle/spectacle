08d363534dcb753582018b20fef11ed13eea49c2
Kemet Dugue <kemet.dugue@gmail.com>
2018-09-13 14:47:49 -0400

    add purchaseDate field

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Order = db.define('order', {
+  purchaseDate: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
   price: {
     type: Sequelize.INTEGER,
     defaultValue: null,
