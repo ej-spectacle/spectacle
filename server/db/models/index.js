6d318477ad7b364d3272bbfaa448c6fc8d726544
Kemet Dugue <kemet.dugue@gmail.com>
2018-09-13 14:46:16 -0400

    remove comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,13 +1,7 @@
 const User = require('./user');
-//const Item = require('./item')
 const Glasses = require('./glasses');
 const Order = require('./order');
 
-//associations
-//Item.belongsTo(Glasses)
-//Item.belongsTo(User)
-//Order.hasMany(Item)
-//Item.belongsTo(Order)
 Order.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(Glasses);
